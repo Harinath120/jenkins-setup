67ac4622b5669812e5b84df788b9cfa8a3127bb3
Rahul Wagh <rahul.wagh17@gmail.com>
2020-10-09 20:00:13 +0300

    delete jenkins script

---
+++ delete-jenkins.sh
@@ -0,0 +1,4 @@
+kubectl delete service k8sdemo-jenkins-service
+kubectl delete deployment k8sdemo-jenkins-deployment
+kubectl delete pvc k8sdemo-jenkins-pvclaim
+kubectl delete pv k8sdemo-jenkins-pv
